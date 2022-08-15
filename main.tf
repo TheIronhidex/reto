c1a6c9cd6fec416a46affe88af83aca7a5e7e9d1
TheIronhidex <92517380+TheIronhidex@users.noreply.github.com>
2022-08-15 20:16:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,8 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "theironhidex"
   ports {
     internal = 80
-    external = 8000
+    external = 87
   }
 }
